5c867a1511a38f38196ceeb4991b0b1771ac46f7
Andres Alvarez <andres15alvarez@gmail.com>
2022-03-08 19:47:26 -0400

    Update texts to spanish and Asilo Digital flow

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,27 +1,27 @@
 export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
-export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
-export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
-export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
+export const WIN_MESSAGES = ['Buen trabajo pana', 'Chevere', 'Klidad']
+export const GAME_COPIED_MESSAGE = 'Copiado al portapapeles'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'Pon mas letras naguara'
+export const WORD_NOT_FOUND_MESSAGE = 'No inventes palabras vale, esa no la tenemos por aca'
 export const HARD_MODE_ALERT_MESSAGE =
-  'Hard Mode can only be enabled at the start!'
+  'Si quieres jugar a lo arrecho es desde el principio, no hagas trampa'
 export const HARD_MODE_DESCRIPTION =
-  'Any revealed hints must be used in subsequent guesses'
-export const HIGH_CONTRAST_MODE_DESCRIPTION = 'For improved color vision'
+  'Cualquier pista revelada debe ser utilizada en las siguientes adivinanzas'
+export const HIGH_CONTRAST_MODE_DESCRIPTION = 'Para que no te duelan esos hermosos ojos'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `The word was ${solution}`
+  `La palabra fue ${solution}`
 export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
-  `Must use ${guess} in position ${position}`
+  `Ira usa ${guess} en la ${position}`
 export const NOT_CONTAINED_MESSAGE = (letter: string) =>
-  `Guess must contain ${letter}`
+  `Debe tener ${letter}`
 export const ENTER_TEXT = 'Enter'
-export const DELETE_TEXT = 'Delete'
-export const STATISTICS_TITLE = 'Statistics'
-export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
-export const NEW_WORD_TEXT = 'New word in'
-export const SHARE_TEXT = 'Share'
-export const TOTAL_TRIES_TEXT = 'Total tries'
-export const SUCCESS_RATE_TEXT = 'Success rate'
-export const CURRENT_STREAK_TEXT = 'Current streak'
-export const BEST_STREAK_TEXT = 'Best streak'
+export const DELETE_TEXT = 'Borrar'
+export const STATISTICS_TITLE = 'Estadisticas'
+export const GUESS_DISTRIBUTION_TEXT = 'Distribucion'
+export const NEW_WORD_TEXT = 'Nueva palabra en'
+export const SHARE_TEXT = 'Compartir'
+export const TOTAL_TRIES_TEXT = 'Total intentos'
+export const SUCCESS_RATE_TEXT = 'Tasa de exito'
+export const CURRENT_STREAK_TEXT = 'Racha actual'
+export const BEST_STREAK_TEXT = 'Mejor racha'
